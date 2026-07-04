2cbae85c29975a2a48f0f41344c0d28b847ebbec
Ryan <ryan7g49i27653@users.noreply.github.com>
2026-07-04 16:06:23 -0400

    Initial commit: MIDI Captain MINI 6 CircuitPython filesystem

---
+++ boot.py
@@ -0,0 +1,23 @@
+import storage
+import board
+import digitalio
+import time
+
+import supervisor
+supervisor.disable_autoreload()
+
+switch = digitalio.DigitalInOut(board.GP1)  # change for the final product
+switch.direction = digitalio.Direction.INPUT
+switch.pull = digitalio.Pull.UP
+time.sleep(0.05)
+
+
+if switch.value is True:
+    storage.disable_usb_drive()
+    storage.remount("/", readonly=False)
+else:
+	storage.remount("/", readonly=False)
+	m = storage.getmount("/")
+	m.label = "MIDICAPTAIN"
+	storage.enable_usb_drive()
+	storage.remount("/", readonly=True) 	
---
+++ code.py
@@ -0,0 +1,1 @@
+import midicaptain6s
